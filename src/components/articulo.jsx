4ca42c558e73049fb20b683f2a12a57daf792af5
sebita876 <sebasvulcanno@gmail.com>
2023-06-08 21:21:35 -0300

    get completo

---
+++ src/components/articulo.jsx
@@ -0,0 +1,9 @@
+
+export function Articulo ({tipo,fecha,id,categoria}){
+    return <tr>
+        <td>{tipo}</td>
+        <td>{fecha}</td>
+        <td>{id}</td>
+        <td>{categoria}</td>
+    </tr>
+}
