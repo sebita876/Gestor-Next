74a42cc2fd1da777c26ee8acff2ad655c7af6b8a
sebita876 <sebasvulcanno@gmail.com>
2023-06-21 10:59:24 -0300

    Desplegable

--- src/bd/models/categorias.js
+++ src/bd/models/categorias.js
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 const categoriaSchema = new mongoose.Schema({
     nombre: {
         type: String,
-        required: true},
-    id:{
-        type:Number,
-        required: true
-    }
+        required: true}
 });
 
 const Categoria = mongoose.models.Categoria || mongoose.model('Categoria', categoriaSchema);
