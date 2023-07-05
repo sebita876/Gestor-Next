d5a95ac08c0cca19fe8133ed0a40da08b96726fe
sebita876 <133991277+sebita876@users.noreply.github.com>
2023-07-05 11:50:51 -0300

    middlewate

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,31 @@
-import Link from "next/link";
-
-const links = [{
-    label: 'Iniciar Sesion',
-    route: '/inventario'
-}, {
-    label: 'Registrarse',
-    route: '/register'
-}]
+'use client'
+import { setCookie } from "nookies";
+import { useRouter } from "next/navigation";
 export default function Base() {
+    const router = useRouter()
+    const handleClick = () => {
+        const user = document.getElementById("user").value
+        const pass = document.getElementById("pass").value
+        // const validar = validarInicio(user,pass)
+        // if(validar==true){
+        //     setCookie(null, "isLogged", "true", {
+        //         maxAge: 3600, // Tiempo de vida de la cookie en segundos
+        //         path: "/", // Ruta de acceso de la cookie
+        //         });
+        //     router.push('/inventario')
+        // }else{
+        //     // document.getElementById("H1 hidden").hidden = false
+        // }
+    }
     return (
         <div>
             <div className='fondo' />
             <div className='fotoAdox' />
             <div className='login'>
                 <div className='container'>
-                    <input type="text" placeholder="Nombre de usuario" className='inputTextLogin' />
-                    <input type="text" placeholder="Contraseña" className='inputTextLogin' />
-                    {links.map(({ label, route }) => (
-                        <p key={route}>
-                            <Link href={route} className="Link">{label}</Link>
-                        </p>
-                    ))}
+                    <input type="text" id="user" placeholder="Nombre de usuario" className='inputTextLogin' />
+                    <input type="text" id="pass" placeholder="Contraseña" className='inputTextLogin' />
+                    <button onClick={()=>handleClick(  ) }> inventario</button>
                 </div>
             </div>
         </div>
---
+++ src/middleware.js
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(request) {
+    // const coockie = request.cookies.get('isLogged');
+    // console.log('Middleware: ', request.nextUrl.pathname);
+    // if (coockie) {
+    //     return NextResponse.next()
+        
+    // }
+    // return NextResponse.redirect(
+    //     new URL(process.env.NEXT_PUBLIC_FRONTEND_URL + '/', request.url)
+    // );
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - favicon.ico (favicon file)
+         */
+        '/inventario',
+        '/register',
+        '/login',
+    ],
+};
